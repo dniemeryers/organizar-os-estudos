6235b763f1b772a26aaf3bb2c0b2fb8b23b416d5
Diego Niemeyer <102764313+dniemeryers@users.noreply.github.com>
2023-03-01 23:26:45 -0300

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  
+  
+} from "react-router-dom";
+
+import {Header}     from './introducao/Header/index.jsx'
+import {Body}       from './introducao/Body/index.jsx'
+import {Inicio}     from './introducao/inicio/index.jsx'
+import { useState } from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  const [dados, setDados] = useState([]);
+
+  function handleSavedados(user){
+    let newDados = [...dados]
+   newDados.push(user)
+   setDados(newDados) 
+   
+  }
+
+ 
+  
+    return (
+      <>
+        <Router> 
+        <Routes>
+            <Route path="/"       element={<Inicio onAddUser={handleSavedados}/>}/>
+            <Route path="/Inicio" element={<Inicio onAddUser={handleSavedados}/>}/>
+            <Route path="/Body"   element={<Body dados={dados}/>}/>
+            <Route path="/Header" element={<Header/>}/>
+          </Routes >
+        </Router>       
+      </>
+    );
+  }
 export default App;
